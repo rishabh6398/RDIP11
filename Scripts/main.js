835a8eef798dc23f9e1fe1c208e2d4c7ca58fd64
rishabh6398 <52570254+rishabh6398@users.noreply.github.com>
2020-09-02 22:01:58 +0530

    Added variables and printed in console

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,2 +1,12 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'JAVASCRIPT INTRODUCTION!';
+myHeading.textContent = 'JAVASCRIPT INTRODUCTION!'
+var a,b;
+a=3;
+b=4;
+function sum(x) {
+	return(x)
+
+	// body...
+}
+console.log(sum(3));
+console.log(sum(4));
