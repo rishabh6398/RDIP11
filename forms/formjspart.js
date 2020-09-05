721c39c97ec2bfddcd8abf7c444c4ad054b58118
rishabh6398 <52570254+rishabh6398@users.noreply.github.com>
2020-09-05 21:57:24 +0530

    Created form and validated it

---
+++ forms/formjspart.js
@@ -0,0 +1,12 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('error')
+
+form.addEventListener('submit',(e)=>{
+	let messages=[]
+	if(number.value.length!=10)
+	{
+		messages.push('Mobile number invalid')
+	}
+	e.preventDefau;t()
+})
